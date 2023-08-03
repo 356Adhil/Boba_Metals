2981988c513e6d99a7381c40d5d8f60c05df7bde
Adhil Akbar <356adhil@gmail.com>
2023-08-04 01:32:51 +0530

    nothing changed in controller.

--- backend/controllers/user/userController.js
+++ backend/controllers/user/userController.js
@@ -3,9 +3,9 @@ const PostsData = require("../../models/postModel");
 const getPosts = async (req, res) => {
   try {
     const postData = await PostsData.find();
-    res.status(200).json(postData)
+    res.status(200).json(postData);
   } catch (error) {
-    res.status(404).json({message:error.message})
+    res.status(404).json({ message: error.message });
   }
 };
 
